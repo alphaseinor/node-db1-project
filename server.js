6be0b92bade7bad41c1f807a71f1efe7ccf7ebd4
Brian Hague <alphaseinor@gmail.com>
2020-01-31 20:55:47 -0600

    accidentally cloned the lambda repo instead of forking paying the price now

--- server.js
+++ server.js
@@ -6,4 +6,31 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+  db('accounts')
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Error on GET account" })
+    })
+  }
+)
+server.get('/:id', (req, res) => {
+  db('accounts')
+    .where({ id: req.params.id })
+    .first()
+    .then(account => {
+      if (account) {
+          res.status(200).json(account);
+      } else {
+          res.status(404).json({ message: "account not available" })
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "error unable to retreive account" })
+    })
+  }
+)
+
 module.exports = server;
